56144943151712ebc1e0013a4111a5c9ce4fe933
stampil <developpeurntic@gmail.com>
2023-02-01 16:11:49 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,6 +5,33 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		//await interaction.reply('Pong!');
+		const row = new ActionRowBuilder()
+			.addComponents(
+				new StringSelectMenuBuilder()
+					.setCustomId('select')
+					.setPlaceholder('Nothing selected')
+					.setMinValues(2)
+					.setMaxValues(3)
+					.addOptions([
+						{
+							label: 'Select me',
+							description: 'This is a description',
+							value: 'first_option',
+						},
+						{
+							label: 'You can select me too',
+							description: 'This is also a description',
+							value: 'second_option',
+						},
+						{
+							label: 'I am also an option',
+							description: 'This is a description as well',
+							value: 'third_option',
+						},
+					]),
+			);
+		await interaction.reply({ content: 'Pong!', components: [row] });
+		
 	},
 };
